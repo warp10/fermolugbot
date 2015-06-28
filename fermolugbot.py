e08250310d81ecbb923b391edb63e2ee812be419
Andrea Colangelo <warp10@debian.org>
2015-06-28 11:44:56 +0200

    Several improvements here and there

--- fermolugbot.py
+++ fermolugbot.py
@@ -27,12 +27,10 @@ import random
 
 
 API_BASE = "https://api.telegram.org/bot121457064:AAG5bEZ2_8KBNYJMuY40HisuZaXluUNbmCg/"
-GROUP_ID = "-21141342" # This is the Test group, then it is updated after the chat id in each new Telegram update
-UPDATES_OFFSET = "184803575" #This should be updated every time the bot is launched, or maybe not, chances are Telegram takes care of it
+UPDATES_OFFSET = "184803575" #FIXME: This needs to be updated every time the bot is added to a new group
 
 def do_get_request(api, params={}):
     r = requests.get(API_BASE + api, params)
-    print(r.text)
     return r.text
 
 def fetch_image(query):
@@ -40,7 +38,7 @@ def fetch_image(query):
     BASE_URL = 'https://ajax.googleapis.com/ajax/services/search/images?'\
                'v=1.0&q=' + query + '&start=%d'
 
-    start = random.randint(0, 10)  # Take a random number for google pagination
+    start = random.randint(0, 60)  # Take a random number for google pagination
     r = requests.get(BASE_URL % start)
     image = json.loads(r.text)['responseData']['results'][random.randint(0, 3)]  # Take a random picture out of the 4 available
     url = image['unescapedUrl']
@@ -49,22 +47,19 @@ def fetch_image(query):
 def send_message(msg):
     do_get_request("sendMessage", params={"chat_id": GROUP_ID, "text": msg})
 
-def milf():
-    print("MILF!")
+def send_milf():
     url = fetch_image("milf")
     send_message(url)
 
-def cameltoe():
-    print("CAMELTOE!")
+def send_cameltoe():
     url = fetch_image("cameltoe")
     send_message(url)
 
-def rustelle():
-    print("RUSTELLE!")
-    url = fetch_image("rustelle")
+def send_rustelle():
+    url = fetch_image("arrosticini")
     send_message(url)
 
-def help():
+def show_help():
     print("HELP!")
     msg = """Il bot del FermoLUG\n\nComandi disponibili:\n/milf: mostra una foto random di MILF\n/cameltoe: mostra una foto random di cameltoe\n/rustelle: mostra una foto random di rustelle"""
     send_message(msg)
@@ -72,18 +67,20 @@ def help():
 while True:
     updates = do_get_request("getUpdates", params={"offset": UPDATES_OFFSET})
     data = json.loads(updates)
+
     for message in data["result"]:
         GROUP_ID = message["message"]["chat"]["id"]
         UPDATES_OFFSET = message["update_id"] + 1
+
         message_text = (message["message"]["text"])
         if message_text.startswith("@FermoLUGbot"):
             message_text = message_text[len("@FermoLUGbot "):]
         if message_text.startswith("/milf"):
-            milf()
+            send_milf()
         if message_text.startswith("/cameltoe"):
-            cameltoe()
+            send_cameltoe()
         if message_text.startswith("/rustelle"):
-            rustelle()
+            send_rustelle()
         if message_text.startswith("/help"):
-            help()
+            show_help()
     sleep(1)
